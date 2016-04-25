c9555dd7c735104ea3598107b53eaccb30824bf3
Thom Denick <tdenick@evox.com>
2016-04-25 12:43:38 -0700

    Added Animated Loading Text (For Animating a UI Text Element with a word and punctuation.)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SGLUsefulScripts
+Useful scripts for Unity.
---
+++ Unity GUI/AnimatedLoadingText.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnimatedLoadingText : MonoBehaviour
+{
+    //private vars
+    [SerializeField] private Text loadCopyText;
+    [SerializeField] string baseLoadingText = "Loading";
+    [SerializeField] string punctuation = ".";
+    [SerializeField] int max_punctuation = 3;
+    [SerializeField] float timeBetweenPeriods;
+
+    int periodIndex;
+
+    //Properties
+
+    //Public Methods
+
+    //Unity Lifecycle
+    private void OnEnable()
+    {
+        StartCoroutine(LoadTextCycle());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(LoadTextCycle());
+    }
+
+    //Private Helper Methods
+    private IEnumerator LoadTextCycle()
+    {
+        periodIndex++;
+        if(periodIndex > max_punctuation) {
+            periodIndex = 0;
+        }
+
+        loadCopyText.text = baseLoadingText;
+
+        for(int i = 0; i < periodIndex; i++) {
+            loadCopyText.text += ". ";
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(LoadTextCycle());
+    }
+}
